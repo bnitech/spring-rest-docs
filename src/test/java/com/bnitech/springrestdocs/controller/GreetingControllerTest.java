06c9e63189372e83d27d9c6f0e5ebfb68e966724
Dongbin <bnitech88@gmail.com>
2022-03-25 19:19:22 +0900

    [dongbin] apply the rest docs

--- src/test/java/com/bnitech/springrestdocs/controller/GreetingControllerTest.java
+++ src/test/java/com/bnitech/springrestdocs/controller/GreetingControllerTest.java
@@ -1,21 +1,40 @@
 package com.bnitech.springrestdocs.controller;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.springframework.restdocs.mockmvc.MockMvcRestDocumentation.document;
+import static org.springframework.restdocs.mockmvc.MockMvcRestDocumentation.documentationConfiguration;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.restdocs.RestDocumentationContextProvider;
+import org.springframework.restdocs.RestDocumentationExtension;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.MvcResult;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
+import org.springframework.web.context.WebApplicationContext;
 
 @SpringBootTest
 @AutoConfigureMockMvc
+@ExtendWith(RestDocumentationExtension.class)
 class GreetingControllerTest {
 
   @Autowired
   private MockMvc mockMvc;
+
+  @BeforeEach
+  void setUp(
+      WebApplicationContext webApplicationContext,
+      RestDocumentationContextProvider restDocumentation
+  ) {
+    this.mockMvc = MockMvcBuilders.webAppContextSetup(webApplicationContext)
+        .apply(documentationConfiguration(restDocumentation))
+        .build();
+  }
 
   @Test
   void 샘플_테스트() throws Exception {
@@ -26,7 +45,7 @@ class GreetingControllerTest {
     // region when
     final MvcResult result = mockMvc.perform(
         get("/greeting").queryParam("name", name)
-    ).andReturn();
+    ).andDo(document("index")).andReturn();
     // endregion
 
     // region then
